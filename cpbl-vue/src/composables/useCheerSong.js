22288d319c7bd28870fe6f64627c9e2c39a05d19
林知葶 <411277001@mail.nknu.edu.tw>
2026-06-10 15:12:05 +0800

    feat: add default cheer song for Chang Yu-cheng

--- cpbl-vue/src/composables/useCheerSong.js
+++ cpbl-vue/src/composables/useCheerSong.js
@@ -19,6 +19,7 @@ const DIRECT_URL_FIELDS = [
 const CHEER_SONGS = {
   // 之後只要補上球員姓名與 YouTube ID，抽到該球員就會直接播放。
   // 例：王威晨: { youtubeId: '影片ID', title: '王威晨 應援曲' }
+  '張育成': { youtubeId: 'ZdAvmv8cXvg', title: '張育成 應援曲' }
 }
 
 const CHEER_OVERRIDE_KEY = 'gobase_cheer_songs'
